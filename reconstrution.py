51c1603c8f4d4a2dbcbab5876a4d2800ef592831
lephoquebleu <younes.chefou@gmail.com>
2020-05-09 02:48:01 +0200

    Ptites notes avant que j'oublie

--- reconstrution.py
+++ reconstrution.py
@@ -6,8 +6,8 @@ import tkinter
 matplotlib.use('TkAgg')
 
 donnees = np.load("arr_0.npy")
-print(donnees)
-print(donnees.shape)
+# print(donnees)
+# print(donnees.shape)
 
 #Tableau à 2 dimensions
 #1ere dimension : n° essai
@@ -28,9 +28,15 @@ for i in range(0, 360):
     cercleY[i] = 5*np.sin(2*np.pi*(i/360))
 
 
-plt.clf()
-plt.plot(cercleX, cercleY)
-plt.show()
+# plt.clf()
+# plt.plot(cercleX, cercleY)
+# plt.show()
+
+#Le principe de notre reconstruction est donc de parcourir notre cercle, et à
+#chaque point du cercle, representer les 1384 points de la figure correspondant lié
+# à ce point
+#Nous allons sauvegarder les données dans un fichier txt pour les observer dans leur entiereté
+np.savetxt('donnees.txt', donnees)
 # figure3D = np.empty((357, 357, 1384), dtype = float)
 #
 # for x in figure3D:
