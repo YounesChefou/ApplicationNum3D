516e38ebf4a21cde9a92e9df2625b804087f9901
lephoquebleu <younes.chefou@gmail.com>
2020-05-11 14:21:17 +0200

    Pistes + image d'une acquisition

--- reconstrution.py
+++ reconstrution.py
@@ -33,10 +33,34 @@ for i in range(0, 360):
 # plt.show()
 
 #Le principe de notre reconstruction est donc de parcourir notre cercle, et à
-#chaque point du cercle, representer les 1384 points de la figure correspondant lié
-# à ce point
+#chaque point du cercle, représenter les points correspondants sur notre nouvelle dimension
+
 #Nous allons sauvegarder les données dans un fichier txt pour les observer dans leur entiereté
 np.savetxt('donnees.txt', donnees)
+
+#Ce qu'il faut faire, c'est de d'abord recupérér les distances captées par le transducteur
+#En partant du principe que la distance est le temps entre deux echos divisé par 340 m/s
+#Chaque essai se repete toutes les 7000us et dure 86.5us
+
+#Contrairement au projet en classe, le transducteur est ici fixe et ne descend pas de haut en bas
+#Nous n'allons donc pas avoir un figure en 3D mais plutôt une tranche vue du dessus
+
+#Pour recuperer les echos, il faut pouvoir les identifier, nous allons donc commencer par afficher la
+#premiere ligne afin d'avoir une meilleure vision de l'acquisition
+
+xvals = range(donnees.shape[1]);
+plt.clf()
+plt.plot(xvals,donnees[0])
+plt.show()
+
+#Voir fichier Acquisition_1.png
+#Toutes les acquisitions ont le même profil, un pic correspondant à l'envoi de l'onde puis
+#un pic plus faible correspondant à la reflexion de l'onde
+
+#Proposition : "Normaliser" l'acquisition en determinant le seuil où un pic correspond à une reflexion
+#de l'onde, toutes les valeurs inférieures seront mises à zéro et celles depassant le seul à 1
+#A tester ce soir
+
 # figure3D = np.empty((357, 357, 1384), dtype = float)
 #
 # for x in figure3D:
